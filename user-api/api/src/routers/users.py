3c88a30d4cdcbd9a2c12d8035790e1671040e3ef
Levi Lutz <levi6900@icloud.com>
2022-05-05 19:01:34 -0400

    Fix docs not showing

---
+++ user-api/api/src/routers/users.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter
+
+from internal import users
+
+import config
+from routers import api_models
+from routers.utils import sanitize_excs
+
+
+router = APIRouter(prefix=config.EXPECTED_PREFIX)
+
+
+@router.get("/register")
+async def register(
+    register_request: api_models.RegisterRequest,
+) -> api_models.SuccessResponse:
+    """Register a new user."""
+    with sanitize_excs():
+        await users.register(
+            username=register_request.username,
+            password=register_request.password,
+        )
+    return api_models.success
