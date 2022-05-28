76136f065faea91a123f33235b00a42b6172c056
Levi Lutz <levi6900@icloud.com>
2022-05-28 15:55:39 -0400

    Make create_migration.sh a tad more verbose
    Specifically, output the full path to the new migration

--- user-api/container/create_migration.sh
+++ user-api/container/create_migration.sh
@@ -13,8 +13,11 @@ fi
 
 c_time=$(date +"%s")
 filename="m_${c_time}_$1.py"
+fullpath="user-api/container/migrations/migrations/$filename"
 echo "Creating migration $filename"
 
-cp user-api/container/migrations/migrations/template.py "user-api/container/migrations/migrations/$filename"
+cp user-api/container/migrations/template.py "$fullpath"
+
+echo "Written to $fullpath"
 
 echo "Done"
