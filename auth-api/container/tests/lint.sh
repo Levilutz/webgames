51fcf19b12af11b34949aaee0cf8c1e29012eab9
Levi Lutz <levi6900@icloud.com>
2022-05-29 00:25:37 -0400

    Add stub unit test

--- auth-api/container/tests/lint.sh
+++ auth-api/container/tests/lint.sh
@@ -7,6 +7,10 @@ mypy auth_api --strict
 black --check --diff auth_api
 flake8 --max-line-length=88 auth_api
 
+# No mypy for tests (gets messy with patching / mocking sometimes)
+black --check --diff tests
+flake8 --max-line-length=88 tests
+
 # shellcheck disable=SC2038
 find . -iname "*.sh" | xargs shellcheck
 echo "Shellcheck successful"
