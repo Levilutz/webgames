3535b4fb019a6d70c6921d9bbd8fd88d537c78e2
Levi Lutz <levi6900@icloud.com>
2022-06-06 23:20:21 -0400

    Stub out email sending service

---
+++ email-api/container/email_api/services/email_utils.py
@@ -0,0 +1,31 @@
+from email.headerregistry import Address
+from email.message import EmailMessage
+from smtplib import SMTP
+from typing import List
+
+from email_api import config
+
+
+def send_email(
+    to_addrs: List[Address], subject: str, content: str, from_user="no-reply"
+) -> None:
+    """Send an email to a given list of recipients."""
+    # Construct the email
+    msg = EmailMessage()
+    msg.set_content(content)
+    msg["Subject"] = subject
+    msg["From"] = Address(
+        display_name=config.MAIL_FROM_DISPLAY,
+        username=from_user,
+        domain=config.MAIL_FROM_DOMAIN,
+    )
+    msg["To"] = to_addrs
+
+    # Send the email
+    with SMTP(config.MAIL_SERVER, port=config.MAIL_SERVER_PORT) as smtp:
+        smtp.send_message(msg)
+
+
+if __name__ == "__main__":
+    dest = Address("Levi Lutz", "levi", "localhost")
+    send_email([dest], "Important information", "I'm some content")
